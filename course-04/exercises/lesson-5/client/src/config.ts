f0b8a6c50729190d8786536dcf80df71947f274b
Elmir Rustemovic <elmir.rustemovic@arvato.com>
2021-12-14 22:57:03 +0100

    lesson 5

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://eg7161bftb.execute-api.eu-central-1.amazonaws.com/dev'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-rjccjlb0.eu.auth0.com',
+  clientId: '1fQQy3helFeMtyNlKLgZ3YaELBZsiLdo',
   callbackUrl: 'http://localhost:3000/callback'
 }
